7696c3efcf7a30ba1c4ef134e30ba6f0b7e6881e
antonio renteria <antrent@gmail.com>
2021-02-02 12:33:36 -0500

    cambios

---
+++ 06_operadores.py
@@ -0,0 +1,22 @@
+# Operadores
+print('Suma:', 3 + 4)
+print('Resta:', 3 - 4)
+print('Multiplicacion:', 3 * 4)
+print('Division entera:', 4 // 3)
+print('Modulo', 4 % 3)
+print('Division decimal: ',4 / 3)
+print('Potencia:' ,3 ** 4)
+
+# Comparadores
+
+print('Igualacion:', 3 == 4)
+print('Diferente:', 3 != 4)
+print('Mayor que:', 3 > 4)
+print('Mayor o igual que:', 4 >= 3)
+print('Menor que', 4 < 3)
+print('Menor o igual que: ',4 <= 3)
+
+# Operadores logicos
+print('AND:', 1 == 3 and 4 != 1)
+print('OR:', 3 != 2 or 4 == 4)
+
---
+++ 06_sentencias_condicionales.py
@@ -0,0 +1,9 @@
+x = 0
+x = 2
+x = -2
+if x == 0:
+    print('x no vale nada')
+elif x > 0:
+    print('x es positivo')
+else:
+    print('x es negativo')
